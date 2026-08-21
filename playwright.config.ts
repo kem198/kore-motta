5aeffd3d0e5236a9225c6c67e92836a7ff5d1ec3
KeM198 <kenkenpa198@gmail.com>
2026-08-22 06:26:17 +0900

    feat: update Playwright configuration for improved trace and video recording settings

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,5 @@
+/* Created by `npm init playwright@latest` */
+
 import { defineConfig, devices } from "@playwright/test";
 
 /**
@@ -29,7 +31,10 @@ export default defineConfig({
     baseURL: "http://localhost:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    // TODO: 推奨のエビデンス記録に揃える？ https://github.com/kem198/kems-todos/pull/31#discussion_r2442717664
+    screenshot: process.env.CI ? "only-on-failure" : "on",
+    video: process.env.CI ? "retain-on-failure" : "on",
+    trace: process.env.CI ? "retain-on-failure" : "on-first-retry",
   },
 
   /* Configure projects for major browsers */
