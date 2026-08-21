ddda7dcd444e29dce6fc079bec0389f7a71fdaa6
KeM198 <kenkenpa198@gmail.com>
2026-08-21 20:48:13 +0900

    refactor: remove obsolete TODO comment for cleaner code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { TodoApp } from "@/features/todo-app";
 export default function Home() {
   return (
     <div className="flex flex-col gap-2">
-      {/* TODO: 表示崩れするので prose-pre:whitespace-pre-wrap を指定している */}
       <main className="max-w-2xl p-8">
         <TodoApp />
       </main>
