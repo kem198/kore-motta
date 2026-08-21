f85d5ec831f6f29d6d8b9587bdbee2910d0ea44c
KeM198 <kenkenpa198@gmail.com>
2026-08-21 20:48:04 +0900

    refactor: replace article with main for semantic HTML structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-2">
       {/* TODO: 表示崩れするので prose-pre:whitespace-pre-wrap を指定している */}
-      <article className="prose prose-blockquote:font-normal prose-pre:whitespace-pre-wrap max-w-2xl">
+      <main className="max-w-2xl p-8">
         <TodoApp />
-      </article>
+      </main>
     </div>
   );
 }
