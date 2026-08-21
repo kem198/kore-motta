95beb7cc4793dc80b316e540f15a1995c0080a0b
KeM198 <kenkenpa198@gmail.com>
2026-08-22 06:47:55 +0900

    feat: consolidate CI workflows by removing Playwright tests workflow and integrating it into the main CI configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,8 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: "http://localhost:3000",
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    /* Evidence for failed tests */
+    screenshot: "only-on-failure",
     trace: "on-first-retry",
   },
 
@@ -71,9 +72,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "npm run start",
+    url: "http://localhost:3000",
+    reuseExistingServer: !process.env.CI,
+  },
 });
