bd593f1ebb2b6c05d4471134825d39f87308482b
KeM198 <kenkenpa198@gmail.com>
2026-08-21 16:59:31 +0900

    feat: remove old layout and page components, integrate EtrianRegistry into Home

--- src/app/(toys)/etrian-calendar/page.tsx
+++
@@ -1,9 +0,0 @@
-import { EtrianRegistry } from "@/app/(toys)/etrian-calendar/_features/registry/etrian-registry";
-
-export default function Home() {
-  return (
-    <div>
-      <EtrianRegistry />
-    </div>
-  );
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { EtrianRegistry } from "@/features/etrian-registry";
+
 export default function Home() {
   return (
-    <div>
-      <main>あ</main>
+    <div className="flex flex-col gap-2">
+      {/* TODO: 表示崩れするので prose-pre:whitespace-pre-wrap を指定している */}
+      <article className="prose prose-blockquote:font-normal prose-pre:whitespace-pre-wrap max-w-2xl">
+        <EtrianRegistry />
+      </article>
     </div>
   );
 }
