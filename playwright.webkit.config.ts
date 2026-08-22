64199448a5da8a2759a06afafbabbe4b9f2ec350
KeM198 <kenkenpa198@gmail.com>
2026-08-22 09:42:18 +0900

    refactor: enhance Playwright test scripts and configuration for improved execution

---
+++ playwright.webkit.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig, devices } from "@playwright/test";
+import base from "./playwright.config";
+
+// webkit で複数ワーカー実行時に落ちてしまいテストが不合格となるため別設定にする
+export default defineConfig({
+  ...base,
+  workers: 1,
+  projects: [{ name: "webkit", use: { ...devices["Desktop Safari"] } }],
+});
