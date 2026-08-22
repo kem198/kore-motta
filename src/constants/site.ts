bf44da2685892c282bc610bf5caeab2d6a1a4611
KeM198 <kenkenpa198@gmail.com>
2026-08-22 21:46:59 +0900

    refactor: update SITE_NAME to "Kore Motta?" for consistency

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,3 +1,3 @@
-export const SITE_NAME = "これ、もった？";
+export const SITE_NAME = "Kore Motta?";
 export const SITE_DESCRIPTION = "一日限定の Todo アプリ";
 export const SITE_URL = "https://kore-motta.kem198.net";
