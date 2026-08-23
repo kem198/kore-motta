371002b207f3a899e9cfe14320d95692a2f34baa
KeM198 <kenkenpa198@gmail.com>
2026-08-23 13:36:20 +0900

    feat: add TodoAppHeader component for improved layout and navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { TodoAppHeader } from "@/components/shared/todo-app-header";
 import { TodoApp } from "@/features/todo-app";
 
 export default function Home() {
   return (
     <div className="md:bg-muted min-h-screen">
-      <main className="bg-background mx-auto min-h-screen w-full max-w-2xl p-8">
-        <section data-testid="todo">
+      <TodoAppHeader />
+
+      <main className="bg-background mx-auto min-h-screen w-full max-w-2xl">
+        <section data-testid="todo" className="p-8">
           <TodoApp />
         </section>
       </main>
