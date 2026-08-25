ed0a2c074b6cc2d24fe5e48beb7fdd357b7cc0c3
KeM198 <kenkenpa198@gmail.com>
2026-08-26 01:28:04 +0900

    fix: update SITE_DESCRIPTION for accurate translation

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,3 +1,3 @@
 export const SITE_NAME = "Kore Motta?";
-export const SITE_DESCRIPTION = "一日限りの Todo アプリ";
+export const SITE_DESCRIPTION = "今日だけの Todo アプリ";
 export const SITE_URL = "https://kore-motta.kem198.net";
