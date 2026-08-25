29cb3237a90be34e569045656f836d9af89f684e
KeM198 <kenkenpa198@gmail.com>
2026-08-26 00:35:09 +0900

    fix: correct typo in SITE_DESCRIPTION for clarity

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,3 +1,3 @@
 export const SITE_NAME = "Kore Motta?";
-export const SITE_DESCRIPTION = "一日限定の Todo アプリ";
+export const SITE_DESCRIPTION = "一日限りの Todo アプリ";
 export const SITE_URL = "https://kore-motta.kem198.net";
