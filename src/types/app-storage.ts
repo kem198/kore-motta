abf64c2012fbbc865a137bf29e3f3e33b0131412
KeM198 <kenkenpa198@gmail.com>
2026-08-25 21:19:17 +0900

    Revert "refactor: simplify appStorage schema by inferring types directly from zod schema"
    This reverts commit 26ae83472620ed4a51388b8de409fa6f40f553fc.

--- src/types/app-storage.ts
+++ src/types/app-storage.ts
@@ -1,4 +1,17 @@
-import { appStorageSchema } from "@/schemas/app-storage-schema";
-import * as z from "zod";
+import { CURRENT_APP_STORAGE_VERSION } from "@/constants/version";
+import { Category } from "@/types/category";
+import { Todo } from "@/types/todo";
 
-export type AppStorage = z.infer<typeof appStorageSchema>;
+export type AppStorageVersion = typeof CURRENT_APP_STORAGE_VERSION;
+
+export type AppStorageData = {
+  /** TODO: 暫定型 */
+  settings: Record<string, unknown>;
+  categories: Category[];
+  todos: Todo[];
+};
+
+export type AppStorage = {
+  version: AppStorageVersion;
+  data: AppStorageData;
+};
