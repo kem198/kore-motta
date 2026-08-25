6ebc0c8914d5b42389eedd98248cb49665188d79
KeM198 <kenkenpa198@gmail.com>
2026-08-26 00:25:42 +0900

    refactor: add timezone configuration for tests to ensure consistency in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,9 +29,11 @@ export default defineConfig({
     baseURL: "http://localhost:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-
     screenshot: "only-on-failure",
     trace: "on-first-retry",
+
+    /* 日付関連のテストで CI が失敗するため日本時間に固定する */
+    timezoneId: "Asia/Tokyo",
   },
 
   /* Configure projects for major browsers */
