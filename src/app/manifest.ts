c09123e66a14b542891db7afe63641db1e5b57e9
KeM198 <kenkenpa198@gmail.com>
2026-08-27 10:38:56 +0900

    fix: update theme color in manifest for improved visual consistency

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -8,7 +8,7 @@ export default function manifest(): MetadataRoute.Manifest {
     display: "standalone",
     display_override: ["window-controls-overlay", "standalone"],
     start_url: "/",
-    theme_color: "#446c6b",
+    theme_color: "#fdfcf9",
     background_color: "#fcfaf6",
     icons: [
       {
