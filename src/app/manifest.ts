ed1cf75ec4a4f058f4b9b19f4f3829e73518be4f
KeM198 <kenkenpa198@gmail.com>
2026-08-27 22:58:29 +0900

    fix: update icon files and manifest for improved asset management

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: "#f2f4f5",
     icons: [
       {
-        src: "/assets/icons/icon-192.png",
+        src: "/assets/icons/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/assets/icons/icon-512.png",
+        src: "/assets/icons/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
