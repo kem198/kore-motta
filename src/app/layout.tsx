40509733d405606ad50670b308adfa92e2c1a59f
KeM198 <kenkenpa198@gmail.com>
2026-08-27 18:43:34 +0900

    feat: add viewport configuration for responsive design in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,19 @@ import { Toaster } from "@/components/ui/sonner";
 import { notoSansJp, ubuntuSans, ubuntuSansMono } from "@/constants/fonts";
 import { SITE_DESCRIPTION, SITE_NAME, SITE_URL } from "@/constants/site";
 import { cn } from "@/lib/utils";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL(SITE_URL),
   title: `${SITE_NAME}`,
   description: SITE_DESCRIPTION,
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  interactiveWidget: "resizes-content",
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
