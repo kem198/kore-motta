878f981e473962a8775f8492437b268a18459ce2
KeM198 <kenkenpa198@gmail.com>
2026-08-28 23:29:03 +0900

    refactor: rename InformationSectionItem type to ListItemWithTitle for consistency

--- src/components/shared/dialog/help-dialog.tsx
+++ src/components/shared/dialog/help-dialog.tsx
@@ -24,7 +24,7 @@ type ListItem = {
   children?: string[];
 };
 
-type InformationSectionItem = {
+type ListItemWithTitle = {
   title: string;
   items: ListItem[];
 };
@@ -64,7 +64,7 @@ const TERMS: ListItem[] = [
   },
 ];
 
-const INFORMATION: InformationSectionItem[] = [
+const INFORMATION: ListItemWithTitle[] = [
   {
     title: "保存先",
     items: [
@@ -239,7 +239,7 @@ function HelpAccordionItem({
   );
 }
 
-type InformationSectionProps = InformationSectionItem & {
+type InformationSectionProps = ListItemWithTitle & {
   className?: string;
 };
 
