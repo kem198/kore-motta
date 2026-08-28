d8922df0145bb5779d87df1764a20fa73bf98ddd
KeM198 <kenkenpa198@gmail.com>
2026-08-28 18:39:49 +0900

    Revert "refactor: enhance TodoFormFooter to support additional props and improve className handling"
    This reverts commit 67bdc5737b5b202da1c683301e7f59ea5ab37dbe.

--- src/components/shared/todo-form-footer.tsx
+++ src/components/shared/todo-form-footer.tsx
@@ -1,24 +1,19 @@
 import { TodoForm } from "@/components/shared/todo-form";
 import { cn } from "@/lib/utils";
 import { TodoFormValues } from "@/schemas/todo-form-schema";
-import { ComponentPropsWithoutRef } from "react";
 
 type TodoFormFooterProps = {
   onSubmit: (values: TodoFormValues) => void;
-} & Omit<ComponentPropsWithoutRef<"footer">, "onSubmit">;
+  className?: string;
+};
 
-export function TodoFormFooter({
-  onSubmit,
-  className,
-  ...props
-}: TodoFormFooterProps) {
+export function TodoFormFooter({ onSubmit, className }: TodoFormFooterProps) {
   return (
     <footer
       className={cn(
-        "bg-background mx-auto w-full max-w-3xl border-t p-4 pb-[calc(1rem+env(safe-area-inset-bottom))]",
+        'bg-background pb-[calc(1rem+env(safe-area-inset-bottom))]" mx-auto w-full max-w-3xl border-t p-4',
         className,
       )}
-      {...props}
     >
       <TodoForm onSubmit={onSubmit} />
     </footer>
