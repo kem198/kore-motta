754eea14e2da69bae0ad8abe666345aad0fdcacb
KeM198 <kenkenpa198@gmail.com>
2026-08-28 18:40:25 +0900

    Revert "fix: update HTMLAttributes type in TodoAppHeader and enhance className support in TodoList"
    This reverts commit 28bb67ef804a3b08a8c2ef8adf2af4c6236ef31f.

--- src/components/shared/todo-list.tsx
+++ src/components/shared/todo-list.tsx
@@ -15,7 +15,6 @@ import { ButtonGroup } from "@/components/ui/button-group";
 import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
 import { MESSAGES } from "@/constants/messages";
-import { cn } from "@/lib/utils";
 import { Category } from "@/schemas/category-schema";
 import { Todo } from "@/schemas/todo-schema";
 import { ChevronDown, ChevronUp, Trash2 } from "lucide-react";
@@ -201,7 +200,6 @@ type TodoListContentProps = {
   onDelete: (todo: Todo) => void;
   onUpdate: (todo: Todo) => void;
   onReorder: (startIndex: number, endIndex: number) => void;
-  className?: string;
 };
 
 function TodoListContent({
@@ -211,10 +209,9 @@ function TodoListContent({
   onDelete,
   onUpdate,
   onReorder,
-  className,
 }: TodoListContentProps) {
   return (
-    <div className={cn("flex w-full flex-col gap-2", className)}>
+    <div className="flex w-full flex-col gap-2">
       <div>{/* 上部の隙間用 */}</div>
       {todos.map((todo, index) => (
         <Fragment key={todo.id}>
@@ -244,7 +241,6 @@ type TodoListProps = {
   onDelete: (todo: Todo) => void;
   onUpdate: (todo: Todo) => void;
   onReorder: (startIndex: number, endIndex: number) => void;
-  className?: string;
 };
 
 export function TodoList({
@@ -255,7 +251,6 @@ export function TodoList({
   onDelete,
   onUpdate,
   onReorder,
-  className,
 }: TodoListProps) {
   if (!isLoaded) {
     return <TodoListLoading />;
@@ -273,7 +268,6 @@ export function TodoList({
       onDelete={onDelete}
       onUpdate={onUpdate}
       onReorder={onReorder}
-      className={className}
     />
   );
 }
