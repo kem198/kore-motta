09306528388df94c5b10ce435e86921c28e29c00
KeM198 <kenkenpa198@gmail.com>
2026-08-28 18:23:09 +0900

    refactor: simplify layout structure in TodoApp and enhance TodoFormFooter with className support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,8 @@ import { TodoApp } from "@/features/todo-app";
 
 export default function Home() {
   return (
-    <div className="relative h-dvh bg-gray-200">
-      <main
-        className="bg-background mx-auto h-full w-full max-w-3xl"
-        data-testid="todo"
-      >
-        <TodoApp />
-      </main>
+    <div className="bg-gray-200">
+      <TodoApp />
     </div>
   );
 }
