d02b546f1918013a1846d71fa058c01c98f16c2d
KeM198 <kenkenpa198@gmail.com>
2026-08-28 18:39:56 +0900

    Revert "fix: add data-testid attribute to the main div in Home component for testing purposes"
    This reverts commit 2ee05c7557b649fcb043d8bba532cd117b34e7cf.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { TodoApp } from "@/features/todo-app";
 
 export default function Home() {
   return (
-    <div className="bg-gray-200" data-testid="todo">
+    <div className="bg-gray-200">
       <TodoApp />
     </div>
   );
