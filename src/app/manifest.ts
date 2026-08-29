56e24703587d977265601f49223e8944da6b7577
KeM198 <kenkenpa198@gmail.com>
2026-08-30 01:42:51 +0900

    fix: update theme and background colors in manifest for consistency

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -7,8 +7,8 @@ export default function manifest(): MetadataRoute.Manifest {
     short_name: SITE_NAME,
     display: "standalone",
     start_url: "/",
-    theme_color: "#435d62",
-    background_color: "#f1f4f5",
+    theme_color: "#496369",
+    background_color: "#f8f9fa",
     icons: [
       {
         src: "/assets/icons/icon-192x192.png",
