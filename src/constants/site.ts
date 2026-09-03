98436187c341cc5b3e158492f5c5dc7ba447408e
KeM198 <kenkenpa198@gmail.com>
2026-09-04 01:25:38 +0900

    feat: update manifest to use dynamic site colors from constants

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,3 +1,6 @@
 export const SITE_NAME = "Kore Motta?";
 export const SITE_DESCRIPTION = "今日だけの Todo アプリ";
 export const SITE_URL = "https://kore-motta.kem198.net";
+
+export const SITE_THEME_COLOR = "#4e4a4e";
+export const SITE_BACKGROUND_COLOR = "#f8f9f9";
